9242d0b779c871fa7ad0a354ad535e60658f9f2f
Petr Doležal <sirelkir@gmail.com>
2022-07-03 21:40:43 +0200

    configs, PCA transform

---
+++ scripts/transform_PCA.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+"""
+Perform Principal Components Analysis (PCA) on the Power Imbalance
+
+Relevant Settings
+-----------------
+
+.. code:: yaml
+
+    pca:
+       
+.. seealso:: #TODO
+    Documentation of the configuration file ``config.yaml`` at :ref:`costs_cf`,
+    :ref:`electricity_cf`, :ref:`load_cf`, :ref:`renewable_cf`, :ref:`lines_cf`
+
+Inputs
+------
+- ``networks/elec.nc``
+
+Outputs
+-------
+- ``networks/pca_comp.nc``
+- ``data/pca_timeseries.nc``
+
+Description
+-----------
+
+The rule :mod:`transform_PCA` ties all the different data inputs from the preceding rules together into a detailed PyPSA network that is stored in ``networks/elec.nc``. It includes:
+
+- today's transmission topology and transfer capacities (optionally including lines which are under construction according to the config settings ``lines: under_construction`` and ``links: under_construction``),
+- today's thermal and hydro power generation capacities (for the technologies listed in the config setting ``electricity: conventional_carriers``), and
+- today's load time-series (upsampled in a top-down approach according to population and gross domestic product)
+
+It further adds extendable ``generators`` with **zero** capacity for
+
+- photovoltaic, onshore and AC- as well as DC-connected offshore wind installations with today's locational, hourly wind and solar capacity factors (but **no** current capacities),
+- additional open- and combined-cycle gas turbines (if ``OCGT`` and/or ``CCGT`` is listed in the config setting ``electricity: extendable_carriers``)
+"""
+
+import numpy as np
+
+from sklearn.decomposition import PCA
+
